1cb77fbaee069b702066ac4255859e1fcda869a7
Oleksandr Tsyhanok <zedicusgd@gmail.com>
2023-10-04 16:56:58 +0300

    add character controller and die animation

--- Assets/Scripts/Combat/CombatTarget.cs
+++ Assets/Scripts/Combat/CombatTarget.cs
@@ -1,9 +1,23 @@
+using System;
 using UnityEngine;
 
 namespace SimpleRPG.Combat
 {
+    [RequireComponent(typeof(Animator))]
     public class CombatTarget : MonoBehaviour
     {
+        public bool IsDead => _health.IsDead;
         
+        [Header("Health Settings")] 
+        [SerializeField] private float _maxHealth;
+        
+        private Health _health;
+
+        private void Awake()
+        {
+            _health = new Health(_maxHealth, GetComponent<Animator>());
+        }
+
+        public void TakeDamage(float damage) => _health.TakeDamage(damage);
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using CharacterController = SimpleRPG.Core.CharacterController;
+
+namespace SimpleRPG.Enemy
+{
+    public class EnemyController : CharacterController
+    {
+        protected override void UpdateAnimator()
+        {
+            
+        }
+    }
+}
