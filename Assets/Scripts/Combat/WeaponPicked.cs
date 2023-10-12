74fb83a03a3477feae30bd2a89426b15488eb9e7
Oleksandr Tsyhanok <zedicusgd@gmail.com>
2023-10-12 19:05:21 +0300

    add respawn for weapons

--- Assets/Scripts/Combat/WeaponPicked.cs
+++ Assets/Scripts/Combat/WeaponPicked.cs
@@ -1,17 +1,20 @@
 using System;
+using SimpleRPG.Core;
 using SimpleRPG.Player;
 using SimpleRPG.ScriptableObjects;
 using UnityEngine;
 
 namespace SimpleRPG.Combat
 {
-    [RequireComponent(typeof(SphereCollider))]
+    [RequireComponent(typeof(SphereCollider), typeof(Recoverable))]
     public class WeaponPicked : MonoBehaviour
     {
         [SerializeField] private WeaponSO _weapon;
+        private Recoverable _recoverable;
         private void Awake()
         {
             GetComponent<SphereCollider>().isTrigger = true;
+            _recoverable = GetComponent<Recoverable>();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -19,7 +22,7 @@ namespace SimpleRPG.Combat
             if (other.TryGetComponent(out PlayerController player))
             {
                 player.SetWeapon(_weapon);
-                Destroy(gameObject);
+                _recoverable.StartRestore(GetComponent<MeshRenderer>());
             }
         }
     }
